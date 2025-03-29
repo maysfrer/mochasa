b6090fb572e462df0bb57dd774ada476ab9fd25b
maysfrer <maysfrer@espol.edu.ec>
2025-03-29 18:55:24 -0500

    fix: deploy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    historyApiFallback: true,
+  },
   resolve: {
     alias: {
       '@common': path.resolve(__dirname, 'src/common'),
